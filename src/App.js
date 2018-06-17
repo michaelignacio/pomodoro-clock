c6565b2f219c6a1e84308eeef3c0b879b18c6fce
Michael Ignacio <mrignacio1@up.edu.ph>
2018-06-17 11:29:33 +0800

    Added needed elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="label-container">
+          <div id="break-label">
+            <p>Break Length</p>
+          </div>
+          <button 
+            id="break-decrement"
+            onClick={() => this.handleClick()} 
+          >
+            Down
+          </button>
+          <div id="break-length">5</div>
+          <button 
+            id="break-increment"
+            onClick={() => this.handleClick()} 
+          >
+            Up
+          </button>
+        </div>
+
+        <div className="label-container">
+          <div id="session-label">
+            Session Length
+          </div>
+          <button 
+            id="session-decrement"
+            onClick={() => this.handleClick()} 
+          >
+            Down
+          </button>
+          <div id="session-length">25</div>
+          <button 
+            id="session-increment"
+            onClick={() => this.handleClick()} 
+          >
+            Up
+          </button>
+        </div>
+
+        <div className="session-timer">
+          <div id="timer-label">Session</div>
+          <div id="time-left">
+
+          </div>
+          <button 
+            id="start_stop"
+            onClick={() => this.handleClick()} 
+          >
+            Start/Stop
+          </button>
+          <button 
+            id="reset"
+            onClick={() => this.handleClick()} 
+          >
+            Reset
+          </button>
+
+        </div>
+
       </div>
     );
   }
